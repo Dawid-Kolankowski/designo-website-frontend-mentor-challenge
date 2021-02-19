0dd5fc58c6837c110f767f5ceaef53d08005ab22
Dawid <nexog1997@gmail.com>
2021-02-19 02:03:36 +0100

    collapse on click outside

--- header.js
+++ header.js
@@ -2,7 +2,14 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const body = document.body;
 const y = window.scrollY;
+const overlay = document.querySelector('.overlay');
 btnHamburger.addEventListener('click', function () {
+  console.log(overlay);
+  header.classList.toggle('open');
+  body.classList.toggle('no-scroll');
+});
+
+overlay.addEventListener('click', function () {
   header.classList.toggle('open');
   body.classList.toggle('no-scroll');
 });
